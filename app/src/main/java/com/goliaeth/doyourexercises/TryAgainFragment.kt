98b3b69f65bcfd1eee63984376135c7e05b60108
Goliaeth <75118259+Goliaeth@users.noreply.github.com>
2022-09-15 09:49:12 +0500

    finish navigation

--- app/src/main/java/com/goliaeth/doyourexercises/TryAgainFragment.kt
+++ app/src/main/java/com/goliaeth/doyourexercises/TryAgainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class TryAgainFragment : Fragment() {
+
+    lateinit var tryAgainButton: Button
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +39,14 @@ class TryAgainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_try_again, container, false)
+        val v = inflater.inflate(R.layout.fragment_try_again, container, false)
+
+        tryAgainButton = v.findViewById(R.id.try_again_button)
+        tryAgainButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_tryAgainFragment_to_exerciseFragment)
+        }
+
+        return  v
     }
 
     companion object {
--- app/src/main/java/com/goliaeth/doyourexercises/WellDoneFragment.kt
+++ app/src/main/java/com/goliaeth/doyourexercises/WellDoneFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +19,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class WellDoneFragment : Fragment() {
+
+    lateinit var wellDoneButton: Button
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +39,14 @@ class WellDoneFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_well_done, container, false)
+        val v = inflater.inflate(R.layout.fragment_well_done, container, false)
+
+        wellDoneButton = v.findViewById(R.id.next_workout_button)
+        wellDoneButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_wellDoneFragment_to_exerciseFragment)
+        }
+
+        return v
     }
 
     companion object {
